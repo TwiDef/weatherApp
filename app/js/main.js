851eaa03ac6f9858cfc05ba12032a258e712adee
Def <deferenseo@gmail.com>
2023-02-04 15:38:59 +0300

    added handler for errors

--- app/js/main.js
+++ app/js/main.js
@@ -20,22 +20,40 @@ form.addEventListener('submit', (e) => {
     fetch(url).then((response) => {
         return response.json();
     }).then((data) => {
-        console.log(data);
 
-        // create data
-        const html = `
-        <div class="card">
-            <h2 class="card__city">${data.location.name} <span>${data.location.country}</span></h2>
-            <div class="card__items">
-                <div class="card__value">${data.current.temp_c}<span>&#176;</span></div>
-                <img class="card__img" src="./images/clouds-and-sun.png" alt="weather">
+        // check on errors
+        if (data.error) {
+            const prevCard = document.querySelector('.card');
+            if (prevCard) prevCard.remove();
+
+            // generate card with error
+            const html = `
+            <div class="card">${data.error.message}</div>
+            `;
+
+            // display card
+            header.insertAdjacentHTML('afterend', html);
+        } else {
+            console.log(data);
+            // delete previos cards
+            const prevCard = document.querySelector('.card');
+            if (prevCard) prevCard.remove();
+
+            // create data
+            const html = `
+            <div class="card">
+                <h2 class="card__city">${data.location.name} <span>${data.location.country}</span></h2>
+                <div class="card__items">
+                    <div class="card__value">${data.current.temp_c}<span>&#176;</span></div>
+                    <img class="card__img" src="./images/clouds-and-sun.png" alt="weather">
+                </div>
+                <div class="card__description">${data.current.condition.text}</div>
             </div>
-            <div class="card__description">${data.current.condition.text}</div>
-        </div>
-        `;
+            `;
 
-        // display card
-        header.insertAdjacentHTML('afterend', html);
+            // display card
+            header.insertAdjacentHTML('afterend', html);
+        }
     });
 
 });
